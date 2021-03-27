abd783643aceff4915cd0556d46b985ea95e3bdf
eNV25 <61089994+eNV25@users.noreply.github.com>
2021-03-27 12:43:56 +0100

    down.sh: fix --reject option

--- down.sh
+++ down.sh
@@ -9,13 +9,13 @@ for line in $(cat ${dirname}/sites.txt); do
     err "Scraping ${line}"
     err "$(echo "Scraping ${line}" | sed 's/./-/g')"
     wget -e robots=off \
-        -U 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)' \
+        -U 'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0' \
         -H -nd -nc -np -p \
         -q --show-progress \
         --recursive \
         --level=1 \
         --accept jpg \
-        -R '*-*,**' \
+        -R '*-*,*_*' \
         -P ${targetfolder} \
         ${line}
     err
